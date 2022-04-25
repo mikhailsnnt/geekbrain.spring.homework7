e477b8f8f51987366522f99d4c3bac69cbf4f386
Mihail Nikitin <nitinin.miha@icloud.com>
2022-04-25 21:51:28 +0300

    Initial commit

---
+++ src/main/java/com/sainnt/gb/homework7springdata/Homework7SpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.sainnt.gb.homework7springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework7SpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework7SpringdataApplication.class, args);
+    }
+
+}
